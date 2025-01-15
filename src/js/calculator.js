e61b58fc195a4e17d6e8e4effec1225bd3097f3c
Mekd7 <physicsmanji@gmail.com>
2025-01-15 20:29:45 +0300

    remove refreasher

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -10,6 +10,7 @@ const topSymbols = ["AC", "+/-", "%"];
 
 const display = document.getElementById("display");
 
+//A+B, A×B, A-B, A÷B
 let A = 0;
 let operator = null;
 let B = null;
@@ -24,17 +25,21 @@ for (let i = 0; i < buttonValues.length; i++) {
     let value = buttonValues[i];
     let button = document.createElement("button");
     button.innerText = value;
-
+    
+    //styling button colors
     if (value == "0") {
         button.style.width = "180px";
-        button.style.gridColumn = "span 2";
-    } else if (rightSymbols.includes(value)) {
+        button.style.gridColumn = "span 2"; //take up 2 columns
+    }
+    else if (rightSymbols.includes(value)) {
         button.style.backgroundColor = "#FF9500";
-    } else if (topSymbols.includes(value)) {        
+    }
+    else if (topSymbols.includes(value)) {        
         button.style.backgroundColor = "#D4D4D2";
         button.style.color = "#1C1C1C";
     }
 
+    //process button clicks
     button.addEventListener("click", function() {
         if (rightSymbols.includes(value)) { 
             if (value == "=") {
@@ -44,48 +49,61 @@ for (let i = 0; i < buttonValues.length; i++) {
                     let numB = Number(B);
 
                     if (operator == "÷") {
-                        display.value = numA / numB;
-                    } else if (operator == "×") {
-                        display.value = numA * numB;
-                    } else if (operator == "-") {
-                        display.value = numA - numB;
-                    } else if (operator == "+") {
-                        display.value = numA + numB;
+                        display.value = numA/numB;
+                    }
+                    else if (operator == "×") {
+                        display.value = numA*numB;
+                    }
+                    else if (operator == "-") {
+                        display.value = numA-numB;
+                    }
+                    else if (operator == "+") {
+                        display.value = numA+numB;
                     }
                     clearAll();
                 }
-            } else {
-                operator = value;
+            }
+            else {
+                operator = value; //÷ × - +
                 A = display.value;
                 display.value = "";
             }
-        } else if (topSymbols.includes(value)) {
+        }
+        else if (topSymbols.includes(value)) { //AC +/- %
             if (value == "AC") {
                 clearAll();
                 display.value = "";
-            } else if (value == "+/-") {
+            }
+            else if (value == "+/-") {
                 if (display.value != "" && display.value != "0") {
-                    if (display.value[0] == "-") {
+                    if (display.value[0] == "-") { //remove -
                         display.value = display.value.slice(1); 
-                    } else {
+                    } else { //add -
                         display.value = "-" + display.value; 
                     }
                 }
-            } else if (value == "%") {
+            }
+            else if (value == "%") {
                 display.value = Number(display.value) / 100;
             }
-        } else {
+        }
+        else { //digits or .
             if (value == ".") {
+                //don't add multiple decimal places
                 if (display.value != "" && !display.value.includes(value)) {
                     display.value += value;
                 }
-            } else if (display.value == "0") {
+            }
+            //not a dot, number instead
+            else if (display.value == "0") {
                 display.value = value;
-            } else {
+            }
+            else {
                 display.value += value;
             }
         }
     });
 
+    //add button to calculator
     document.getElementById("buttons").appendChild(button);
 }
